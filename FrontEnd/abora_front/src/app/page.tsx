55b42a3daf3e7300925271699a20e22d33460fd3
chaeyoung <happy7yong83@gmail.com>
2025-09-22 02:19:58 +0900

    style : global css 수정

--- FrontEnd/abora_front/src/app/page.tsx
+++ FrontEnd/abora_front/src/app/page.tsx
@@ -5,11 +5,17 @@ import { useEffect, useState } from 'react';
 import styles from './page.module.css';
 import HoverScramble from './Components/GSAP/TextScramble';
 import LoaderGridfrom from './Components/GSAP/LoaderGridfrom';
+import SplitText from "@/app/Components/ReactBits/TextSplit";
+import CircularText from "@/app/Components/ReactBits/CircularText";
 
 export default function Home() {
     const router = useRouter();
     const [showLoader, setShowLoader] = useState(false);
     const [hasMounted, setHasMounted] = useState(false);
+
+    const handleAnimationComplete = () => {
+        console.log('All letters have animated!');
+    };
 
     // ① 화면이 마운트된 이후임을 표시
     useEffect(() => {
@@ -26,26 +32,45 @@ export default function Home() {
 
     return (
         <>
-            {/*{hasMounted && showLoader && <LoaderGridfrom />}*/}
-
-            <div className={styles.container}>
-                <div className={styles.card}>
-                    <div className={styles.Maintitle}>
-                        <h2 className={styles.title}>MEET ECHO</h2>
-                        <h3 className={styles.subtitle}>생각을 더 묻고, 질문을 더 멀리.</h3>
-                    </div>
-                    <p className={styles.text}>
-                        Echo는 단순한 Q&amp;A 도구가 아닙니다.<br />
-                        하나의 아이디어가 여러 시선 속에서 울림을 만들도록,<br />
-                        AI 아바타들과 함께 질문을 나누고,<br />
-                        다양한 관점으로 생각을 확장하는 대화형 아이디어 실험실입니다.<br /><br />
-                        그냥 당신의 아이디어에 <span className={styles.highlight}>한 줄만</span> 말해주세요.
-                    </p>
-                    <button className={styles.button} onClick={handleButtonClick}>
-                        <HoverScramble from="ENTER" to="GO" />
-                    </button>
-                </div>
+        <div className={styles.container}>
+            <div className={styles.Maintitle}>
+                <CircularText
+                    text="THINK ✦ DEEPER ✦ QUESTION ✦ FURTHER ✦ THINK ✦ DEEPER ✦ QUESTION ✦ FURTHER ✦ TEAMTALK ✦ "
+                    onHover="slowDown"
+                    spinDuration={40}
+                    className={styles.backgroundCircle}
+                />
             </div>
-        </>
-    );
+            <div className={styles.foreground}>
+                <SplitText
+                    text="Hello, Tatak!"
+                    className="text-2xl font-semibold text-center"
+                    delay={100}
+                    duration={0.6}
+                    ease="power3.out"
+                    splitType="chars"
+                    from={{opacity: 0, y: 40}}
+                    to={{opacity: 1, y: 0}}
+                    threshold={0.1}
+                    rootMargin="-100px"
+                    textAlign="center"
+                    onLetterAnimationComplete={handleAnimationComplete}
+                />
+                <h3 className={styles.subtitle}>생각을 더 묻고, 질문을 더 멀리.</h3>
+                <p className={styles.text}>
+
+                    Tatak은 단순한 Q&amp;A 도구가 아닙니다.<br/>
+                    하나의 아이디어가 여러 시선 속에서 울림을 만들도록,<br/>
+                    AI 아바타들과 함께 질문을 나누고,<br/>
+                    다양한 관점으로 생각을 확장하는 대화형 아이디어 실험실입니다.<br/><br/>
+                    그냥 당신의 아이디어에 <span className={styles.highlight}>한 줄만</span> 말해주세요.
+                </p>
+                <button className={styles.button} onClick={handleButtonClick}>
+                    <HoverScramble from="ENTER" to="GO"/>
+                </button>
+            </div>
+        </div>
+</>
+)
+    ;
 }
