a80e8f7a0e97c276b5a9a5b43e1e7f0d0977bf38
chaeyoung <happy7yong83@gmail.com>
2025-09-21 21:22:03 +0900

    refact & style : TitleText style flex 등 변경

--- FrontEnd/abora_front/src/app/Components/TitleTextBlack/TitleTextBlock.tsx
+++ FrontEnd/abora_front/src/app/Components/TitleTextBlack/TitleTextBlock.tsx
@@ -1,14 +1,25 @@
 import style from "./TitleTextBlock.module.css"
 
-interface TitleTextBlackProps{
-    text:string;
-    color?:string;
+interface TitleTextBlockProps {
+    text?: string;       // 기존 문자열
+    textColor?: string;
+    backgroundColor? : string;
+    children?: React.ReactNode; // 추가
 }
 
-export default function TitleTextBlock({text,color="white"}:TitleTextBlackProps){
+export default function TitleTextBlock({
+                                           text,
+                                           textColor = "white",
+                                           children,
+                                           backgroundColor = "black"
+                                       }: TitleTextBlockProps) {
     return (
-        <>
-            <div className={style.textWrapper} style={{color: color}}>{text}</div>
-        </>
-    )
+        <div
+            className={style.textWrapper}
+            style={{color: textColor, backgroundColor: backgroundColor}}
+        >
+            {text && <span className={style.text}>{text}</span>}
+            {children}
+        </div>
+    );
 }
