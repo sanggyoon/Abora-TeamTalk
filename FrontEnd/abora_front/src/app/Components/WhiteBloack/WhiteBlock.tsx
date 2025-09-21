99d5309aba77149c162533e257ee7a54346e4ef2
chaeyoung <happy7yong83@gmail.com>
2025-09-21 21:21:53 +0900

    refact & style : whiteBlock style flex 등 변경

--- FrontEnd/abora_front/src/app/Components/WhiteBloack/WhiteBlock.tsx
+++ FrontEnd/abora_front/src/app/Components/WhiteBloack/WhiteBlock.tsx
@@ -2,21 +2,27 @@
 "use client";
 
 import Image from "next/image";
-import style from "./WhiteBlock.module.css"
+// import common from "@/app/Components/common/common.module.css"
+import styles from "./WhiteBlock.module.css"
 
 interface WhiteBlockProps {
-    imageSrc: string;      // 이미지 경로 (로컬 / 외부 URL)
-    imageAlt?: string;     // 대체 텍스트
-    title: string;         // 큰 제목
+    as? : "div" | "button";
+    onClick?: () => void,
+    imageSrc: string;
+    imageAlt?: string;
+    title: string;
+    style?: React.CSSProperties,
 }
 
 export default function WhiteBlock({
+                                       as : Tag = "div",
                                        imageSrc,
                                        imageAlt = "image",
                                        title,
+                                       onClick,style
                                    }: WhiteBlockProps) {
     return (
-        <div className={style.background}>
+        <Tag className={styles.roleBackground} style={style} onClick={onClick}>
             <Image
                 src={imageSrc}
                 alt={imageAlt}
@@ -27,6 +33,6 @@ export default function WhiteBlock({
             <h3 style={{ marginTop: "12px", fontSize: "1.2rem", fontWeight: "600",color:"black" }}>
                 {title}
             </h3>
-        </div>
+        </Tag>
     );
 }
