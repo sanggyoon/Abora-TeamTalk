d533c51547c6c4c9366d83244c96f37723f41382
chaeyoung <happy7yong83@gmail.com>
2025-09-21 21:22:42 +0900

    refact & style : 온보딩2 페이지 수정

--- FrontEnd/abora_front/src/app/OnboardingSecondPage/page.tsx
+++ FrontEnd/abora_front/src/app/OnboardingSecondPage/page.tsx
@@ -1,50 +1,65 @@
 "use client";
 
-import WhiteBlock from "@/app/Components/WhiteBloack/WhiteBlock";
+// import WhiteBlock from "@/app/Components/WhiteBloack/WhiteBlock";
 import style from "./page.module.css"
 import TitleTextBlock from "@/app/Components/TitleTextBlack/TitleTextBlock";
 import {useRouter} from "next/navigation";
+import SenarioBlock from "@/app/Components/SenarioBlock/SenarioBlock";
+import {useState} from "react";
+import scenarios from "@/data/senario.json"
+import common from "@/app/Components/common/common.module.css"
 
-export default function OnboardingSecondPage() {
-    // const router = useRouter();
-    //
-    // const handleButtonClick = () => {
-    //     setTimeout(() => {
-    //         router.push('/OnboardingSecondPage');
-    //     }, 1000); // 애니메이션 지속시간과 맞추기
-    // };
+export default function OnboardingFirstPage() {
+    const router = useRouter();
+    const [activeList, setActiveList] = useState([false, false, false]);
+
+    const toggle = (index: number) => {
+        setActiveList((prev) =>
+            prev.map((item, i) => (i === index ? !item : item))
+        );
+    };
+
+
+    const handleButtonClick = () => {
+        setTimeout(() => {
+            router.push('/OnboardingSecondPage');
+        }, 1000); // 애니메이션 지속시간과 맞추기
+    };
 
     return (
-        <>
-            <h3 className={style.headerText}>당신은 어떤 사람인가요?</h3>
-            <section>
-                <h3 className={style.headerText}>당신은 직군을 선택하세요</h3>
+        <div className={common.container}>
+            <h3 className={style.headerText}>오늘은 어떤 상황을 연습해볼까요?</h3>
+            <div className={style.container}>
+                <h3 className={style.headerText}>시나리오를 선택하기</h3>
                 <div className={style.roleSection}>
-                    <WhiteBlock
-                        imageSrc="/iconImage/roleicon1.png"
-                        title="개발자"
-                    />
-                    <WhiteBlock
-                        imageSrc="/iconImage/roleicon1.png"
-                        title="개발자"
-                    />
-                    <WhiteBlock
-                        imageSrc="/iconImage/roleicon1.png"
-                        title="개발자"
-                    />
-                    <WhiteBlock
-                        imageSrc="/iconImage/roleicon1.png"
-                        title="개발자"
-                    />
+                    {scenarios.map((s, idx) => (
+                        <SenarioBlock
+                            key={idx}
+                            as="button"
+                            title={s.title}
+                            level={s.level}
+                            imageSrc={s.imageSrc}
+                            description={s.description}
+                            goal={s.goal}
+                            points={s.points}
+                            onClick={() => toggle(idx)}
+                            inlineStyle={{ border: `2px solid ${activeList[idx] ? "var(--select-color)" : "white"}`}}
+
+                        />
+                    ))}
                 </div>
-            </section>
-
-            {/*이름입력*/}
-            <section>
-                <TitleTextBlock text={"이름"}/>
-            </section>
+            </div>
 
 
-        </>
+            {/*다음페이지로 ->버튼*/}
+            <div className={style.textCenter}>
+                <TitleTextBlock>
+                    <button onClick={handleButtonClick} style={{backgroundColor : "black", border:"none"}}>
+                        <p style={{color: "white"}}>인사하러 가기</p>
+                    </button>
+                </TitleTextBlock>
+                <p className={style.buttonDesc}>준비가 완료 되었네요! 함께할 동료에게 인사하러 가볼까요?</p>
+            </div>
+        </div>
     );
 }
