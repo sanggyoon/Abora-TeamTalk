b0e79548184ab7d65e4acaa44f934eed61e5b315
chaeyoung <happy7yong83@gmail.com>
2025-09-28 23:09:13 +0900

    doc : 파일 로그 변경

--- FrontEnd/abora_front/src/app/ChooseAgent/page.tsx
+++ FrontEnd/abora_front/src/app/ChooseAgent/page.tsx
@@ -42,6 +42,7 @@ export default function ChooseAgent() {
 
   return (
       <>
+        <LoaderGridto/>
     <div className={styles.chooseAgentContainer}>
       {/* 슬라이드 A */}
       <div className={styles.chooseAgent_A}>
