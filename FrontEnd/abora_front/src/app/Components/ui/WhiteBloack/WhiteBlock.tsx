59b5aebeebe665ec1993aedf3d8bafabd3b24fdc
chaeyoung <happy7yong83@gmail.com>
2025-10-08 19:57:51 +0900

    feat : config에서 역할에 따른 설정값 가져옴

--- FrontEnd/abora_front/src/app/Components/ui/WhiteBloack/WhiteBlock.tsx
+++ FrontEnd/abora_front/src/app/Components/ui/WhiteBloack/WhiteBlock.tsx
@@ -4,34 +4,33 @@
 import Image from "next/image";
 // import common from "@/app/Components/common/common.module.css"
 import styles from "./WhiteBlock.module.css"
+import {RoleConfig} from "@/app/config/RoleConfig";
+import {Role} from "@/app/types/enum";
 
 interface WhiteBlockProps {
     as? : "div" | "button";
+    role : Role,
     onClick?: () => void,
-    imageSrc: string;
-    imageAlt?: string;
-    title: string;
     style?: React.CSSProperties,
 }
 
 export default function WhiteBlock({
                                        as : Tag = "div",
-                                       imageSrc,
-                                       imageAlt = "image",
-                                       title,
+    role,
                                        onClick,style
                                    }: WhiteBlockProps) {
+    const roleData = RoleConfig[role]
     return (
         <Tag className={styles.roleBackground} style={style} onClick={onClick}>
             <Image
-                src={imageSrc}
-                alt={imageAlt}
+                src={roleData.IconImageSrc}
+                alt={roleData.role}
                 width={100}
                 height={100}
                 style={{ borderRadius: "8px" }}
             />
             <h3 style={{ marginTop: "12px", fontSize: "1.2rem", fontWeight: "600",color:"black" }}>
-                {title}
+                {roleData.role}
             </h3>
         </Tag>
     );
