7a224b0d18fe24118d5b9cafbf0a84547f654699
chaeyoung <happy7yong83@gmail.com>
2025-10-08 18:36:30 +0900

    refactor : BadgeComponent 생성

---
+++ FrontEnd/abora_front/src/app/Components/ui/BadgeComponent/BadgeComponent.tsx
@@ -0,0 +1,29 @@
+import styles from "./BadgeComponent.module.css"
+import React from "react";
+
+interface RoleBadgeProps{
+    color : string;
+    name : string;
+    role : string;
+    iconSrc : string;
+}
+
+//1. Badge 기본적인 컴포넌트
+//2. json에서 역할별 설정 Config에서 받아오는 새 역할별 roleBadge컴포넌트 생성
+//3. AvatarBlock에서 역할 별 roleBadge 컴포넌트를 가져오기
+
+export default function BadgeComponent({color="black", name="defalut", role="defalut", iconSrc=""}:RoleBadgeProps){
+    return (
+        <div>
+            <div className={styles.nameImg}>
+                <img src={iconSrc} alt={`${name} 아이콘`} className={styles.icon}/>
+                <span className={styles.name} style={{color:color}}>
+                    {name}
+                </span>
+            </div>
+            <span className={styles.role}>
+                {role}
+            </span>
+        </div>
+    );
+}
