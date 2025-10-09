10f94175cd9d4b7f1727615195684ff1f87eee2c
chaeyoung <happy7yong83@gmail.com>
2025-10-09 22:24:28 +0900

    refactor : 방생성 메서드 추가 (임시)

---
+++ FrontEnd/abora_front/src/app/ConversationRoom/hooks/useChatSession.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Role} from "@/app/types/enum";
+import {ChatSession} from "@/app/types/interface";
+
+//Session 생성
+export function useChatSession() {
+    const [session, setSession] = useState<ChatSession | null>(null);
+
+    // 페이지 로드 시 localStorage에서 복원
+    useEffect(() => {
+        const savedSession = localStorage.getItem("chatSession");
+        if (savedSession) {
+            const parsed: ChatSession = JSON.parse(savedSession);
+            setSession(parsed);
+        }
+    }, []);
+
+    // 새로운 세션 생성 및 저장
+    const createSession = (id: number, role: Role, room_name:string) => {
+        const newSession: ChatSession = {
+            session_id: id,
+            sender_role: role,
+            is_user: true,
+            room_name : room_name
+        };
+        localStorage.setItem("chatSession", JSON.stringify(newSession));
+        setSession(newSession);
+    };
+
+    return {
+        sessionId: session?.session_id ?? null,
+        senderRole: session?.sender_role ?? null,
+        isUser: session?.is_user ?? true,
+        createSession,
+    };
+}
